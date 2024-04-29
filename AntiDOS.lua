2c6edb7677edd73b7685a517490de532be54b13b
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-04-29 16:44:41 +0500

    Create AntiDOS.lua

---
+++ AntiDOS.lua
@@ -0,0 +1,27 @@
+local file={"hacks.dll"}
+     string={}
+local txtSt="THIS FILE CANNOT BE RUN IN DOS MODE"
+local E = {
+  'a','b','x','23@^&$#'
+}
+local symbols = {require("DOSSb")}
+DOSSb.create()
+DOSSb.getNormalSymbols()
+DOSSb.protect()
+local functions = [[
+rtfile(ty0pe) == 'flrx'
+if flrx = '1' then function off()
+oj(ieieie)
+local flrfunction airfly()
+getFly.mojang()
+local flrfunction airjump()
+getJump.mojangEntityJump()
+limitJump(999999,"UNLIMITED")
+local flrfunction xray()
+getSource("xray.texturepack..mcpedlorg")
+key={"M"}
+--.. current hacks in flarex ..--
+airfly()
+airjump()
+xray()
+]]
